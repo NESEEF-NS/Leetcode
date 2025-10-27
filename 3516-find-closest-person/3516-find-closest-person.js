ff38f62cd26e344b2c1366cfeae82ee18d0444c7
NESEEF P <neseefns@gmail.com>
2025-10-27 15:59:37 +0530

    Time: 0 ms (100%), Space: 56.9 MB (6.14%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    if(Math.abs(z-y) > Math.abs(z-x)){
+        return 1
+    }else if(Math.abs(z-y) < Math.abs(z-x)){
+        return 2
+    }else return 0;
+};
